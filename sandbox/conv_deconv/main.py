d4f2e4045414fbb2ad5fbc642e5dd8f1d3125a00
WheatleyTheCore <alex.colwell@icloud.com>
2026-01-14 09:31:39 -0500

    added conv_deconv sandbox example

---
+++ sandbox/conv_deconv/main.py
@@ -0,0 +1,32 @@
+from scipy import signal
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+t = np.linspace(0, 20, 500)
+
+
+sig1 = signal.square(t)
+sig2 = signal.sawtooth(t)
+
+
+conv_sig = signal.convolve(sig1, sig2)
+
+
+recovered, remainder = signal.deconvolve(conv_sig, sig2)
+
+fig, (ax_orig, ax_win, ax_filt, ax_rec) = plt.subplots(4, 1, sharex=True)
+ax_orig.plot(sig1)
+ax_orig.set_title('Original pulse')
+ax_orig.margins(0, 0.1)
+ax_win.plot(sig2)
+ax_win.set_title('Filter impulse response')
+ax_win.margins(0, 0.1)
+ax_filt.plot(conv_sig)
+ax_filt.set_title('Convolution signal')
+ax_filt.margins(0, 0.1)
+ax_rec.plot(recovered)
+ax_rec.set_title('Recovered input signal')
+ax_rec.margins(0, 0.1)
+fig.tight_layout()
+fig.savefig('output.png')
