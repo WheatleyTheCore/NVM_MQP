626a1b92395df16b842e80af2561c947d752efdd
WheatleyTheCore <alex.colwell@icloud.com>
2026-01-16 14:14:45 -0500

    conv deconv sandbox now uses realistic IR

--- sandbox/conv_deconv/main.py
+++ sandbox/conv_deconv/main.py
@@ -7,8 +7,7 @@ t = np.linspace(0, 20, 500)
 
 
 sig1 = signal.square(t)
-sig2 = signal.sawtooth(t)
-
+sig2 = signal.gausspulse(t[:20], fc=5)
 
 conv_sig = signal.convolve(sig1, sig2)
 
@@ -19,7 +18,7 @@ fig, (ax_orig, ax_win, ax_filt, ax_rec) = plt.subplots(4, 1, sharex=True)
 ax_orig.plot(sig1)
 ax_orig.set_title('Original pulse')
 ax_orig.margins(0, 0.1)
-ax_win.plot(sig2)
+ax_win.plot(np.append(sig2, np.zeros(500-20)))
 ax_win.set_title('Filter impulse response')
 ax_win.margins(0, 0.1)
 ax_filt.plot(conv_sig)
